fb0e8a5b886fe46d1c1ef63e3badb3eba1ee107d
me7alix <me7alix@gmail.com>
2026-03-27 20:40:40 +0300

    refactor & add json support (serialization and deserialization)

--- src/stdlib.c
+++ src/stdlib.c
@@ -12,82 +12,301 @@
 		return VNONE; \
 	} while(0)
 
-void val_sprint_f(Val v, char *buf, int depth) {
+void val_sprint(Val v, StringBuilder *sb, int depth) {
 	switch (v.kind) {
-	case VAL_NONE:  sprintf(buf, "none");                 break;
-	case VAL_INT:   sprintf(buf, "%lli", v.as.vint);      break;
-	case VAL_FLOAT: sprintf(buf, "%lf", v.as.vfloat);     break;
-	case VAL_BOOL:  sprintf(buf, "%s", bstr(v.as.vbool)); break;
+	case VAL_NONE:  sb_appendf(sb, "none");                 break;
+	case VAL_INT:   sb_appendf(sb, "%lli", v.as.vint);      break;
+	case VAL_FLOAT: sb_appendf(sb, "%lf", v.as.vfloat);     break;
+	case VAL_BOOL:  sb_appendf(sb, "%s", bstr(v.as.vbool)); break;
 
 	case VAL_RUNE:
 		char out[4];
 		int n = utf8_encode(v.as.vrune, out);
-		sprintf(buf, "%.*s", n, out);
+		sb_appendf(sb, "%.*s", n, out);
 		break;
 
 	case VAL_FIELD:
 		if (depth == 0)
-			sprintf(buf, "%s", v.as.field);
+			sb_appendf(sb, "%s", v.as.field);
 		else
-			sprintf(buf, ".%s", v.as.field);
+			sb_appendf(sb, ".%s", v.as.field);
 		break;
 
 	case VAL_STR:
 		if (depth == 0)
-			sprintf(buf, "%s", VSTR(v)->items);
+			sb_appendf(sb, "%s", VSTR(v)->items);
 		else
-			sprintf(buf, "\"%s\"", VSTR(v)->items);
+			sb_appendf(sb, "\"%s\"", VSTR(v)->items);
 		break;
 
 	case VAL_LIST: {
-		StringBuilder sb = {0};
-		sb_appendf(&sb, "[");
+		sb_appendf(sb, "[");
 
 		da_foreach (Val, it, VLIST(v)) {
-			char buf[4096]; val_sprint_f(*it, buf, depth + 1);
-			sb_appendf(&sb, "%s", buf);
+			val_sprint(*it, sb, depth + 1);
 			if (it - VLIST(v)->items != VLIST(v)->count - 1)
-				sb_appendf(&sb, ", ", buf);
+				sb_appendf(sb, ", ");
 		}
 
-		sb_appendf(&sb, "]");
-		sprintf(buf, "%s", sb.items);
-		sb_free(&sb);
+		sb_appendf(sb, "]");
 	} break;
 
 	case VAL_DICT: {
-		StringBuilder sb = {0};
 		ValDict *dict = VDICT(v);
 		size_t count = 0;
 
-		sb_appendf(&sb, "{");
+		sb_appendf(sb, "{");
 		ht_foreach_node (ValDict, dict, kv) {
-			char buf[4096];
-			val_sprint_f(kv->key, buf, depth + 1);
-			sb_appendf(&sb, "%s: ", buf);
-			val_sprint_f(kv->val, buf, depth + 1);
-			sb_appendf(&sb, "%s", buf);
+			val_sprint(kv->key, sb, depth + 1);
+			sb_appendf(sb, ": ");
+			val_sprint(kv->val, sb, depth + 1);
 			if (count++ < dict->count - 1)
-				sb_appendf(&sb, ", ");
+				sb_appendf(sb, ", ");
 		}
 
-		sb_appendf(&sb, "}");
-		sprintf(buf, "%s", sb.items);
-		sb_free(&sb);
-	} break;
+		sb_appendf(sb, "}");
+		} break;
 	}
 }
 
-void val_sprint(Val v, char *buf) {
-	val_sprint_f(v, buf, 0);
+void JsonDeserializeF(
+	StringBuilder *sb,
+	Val v,
+
+	long long intend,
+	long long spaces
+) {
+#define ADD_INTENDATION() \
+	do { if (spaces != 0) sb_appendf(sb, "%*s", intend, ""); } while(0)
+
+	switch (v.kind) {
+	case VAL_NONE:  sb_appendf(sb, "null");                   break;
+	case VAL_INT:   sb_appendf(sb, "%lli", v.as.vint);        break;
+	case VAL_FLOAT: sb_appendf(sb, "%lf", v.as.vfloat);       break;
+	case VAL_BOOL:  sb_appendf(sb, "%s", bstr(v.as.vbool));   break;
+	case VAL_STR:   sb_appendf(sb, "\"%s\"", VSTR(v)->items); break;
+
+	case VAL_LIST: {
+		intend += spaces;
+		sb_appendf(sb, "[");
+		if (spaces != 0)
+			sb_appendf(sb, "\n");
+
+		da_foreach (Val, it, VLIST(v)) {
+			ADD_INTENDATION();
+			JsonDeserializeF(sb, *it, intend, spaces);
+			if (it - VLIST(v)->items != VLIST(v)->count - 1) {
+				if (spaces == 0) sb_appendf(sb, ", ");
+				else             sb_appendf(sb, ",\n");
+			}
+		}
+
+		if (spaces != 0)
+			sb_appendf(sb, "\n");
+
+		intend -= spaces;
+		ADD_INTENDATION();
+		sb_appendf(sb, "]");
+	} break;
+
+	case VAL_DICT: {
+		ValDict *dict = VDICT(v);
+		size_t count = 0;
+
+		intend += spaces;
+		sb_appendf(sb, "{");
+		if (spaces != 0)
+			sb_appendf(sb, "\n");
+
+
+		ht_foreach_node (ValDict, dict, kv) {
+			ADD_INTENDATION();
+			JsonDeserializeF(sb, kv->key, intend, spaces);
+			sb_appendf(sb, ": ");
+			JsonDeserializeF(sb, kv->val, intend, spaces);
+			if (count++ < dict->count - 1) {
+				if (spaces == 0) sb_appendf(sb, ", ");
+				else             sb_appendf(sb, ",\n");
+			}
+		}
+
+		if (spaces != 0)
+			sb_appendf(sb, "\n");
+
+		intend -= spaces;
+		ADD_INTENDATION();
+		sb_appendf(sb, "}");
+		} break;
+	}
+#undef ADD_INTENDATION
 }
 
-Val BitXor(EvalCtx *ctx, Location call_loc, Vals args) {
+Val JsonDeserialize(EvalCtx *ctx, Location cloc, Vals args) {
+	if (args.count < 1 || args.count > 2)
+		err(ctx, cloc, "accepts only 1 or 2 arguments");
+
+	bool two_args = args.count == 2;
+	if (two_args == 2 && args.items[1].kind != VAL_INT)
+		err(ctx, cloc, "int expected as second argument");
+
+	long long spaces = 0;
+	if (two_args) spaces = args.items[1].as.vint;
+
+	Val str = eval_new_heap_val(ctx, VAL_STR);
+	JsonDeserializeF(VSTR(str), args.items[0], 0, spaces);
+	return str;
+}
+
+typedef struct {
+	EvalCtx *ectx;
+	Lexer *lex;
+	bool got_err;
+} JsonParserCtx;
+
+Val json_obj(JsonParserCtx *ctx, Val res) {
+	Val obj = eval_new_heap_val(ctx->ectx, VAL_DICT);
+	Val res_fld = {.kind = VAL_FIELD, .as.field = "res"};
+	Val err_fld = {.kind = VAL_FIELD, .as.field = "err"};
+	ValDict_add(VDICT(obj), res_fld, res);
+	ValDict_add(VDICT(obj), err_fld, VNONE);
+	return obj;
+}
+
+Val json_err(JsonParserCtx *ctx, char *msg) {
+	ctx->got_err = true;
+
+	Val err     = eval_new_heap_val(ctx->ectx, VAL_DICT);
+	Val res_fld = {.kind = VAL_FIELD, .as.field = "res"};
+	Val err_fld = {.kind = VAL_FIELD, .as.field = "err"};
+	Val err_msg = eval_new_heap_val(ctx->ectx, VAL_STR);
+
+	size_t lines = ctx->lex->cur_loc.line_num + 1;
+	size_t chars = ctx->lex->cur_loc.line_char - ctx->lex->cur_loc.line_start + 1;
+	sb_appendf(VSTR(err_msg), "%zu:%zu: %s", lines, chars, msg);
+
+	ValDict_add(VDICT(err), res_fld, VNONE);
+	ValDict_add(VDICT(err), err_fld, err_msg);
+	return err;
+}
+
+Val parse_json(JsonParserCtx *ctx) {
+	switch (lexer_peek(ctx->lex).kind) {
+	case TOK_ID: {
+		char *id = lexer_peek(ctx->lex).data;
+		if (strcmp(id, "null") == 0) {
+			lexer_next(ctx->lex);
+			return (Val){.kind = VAL_NONE};
+		}	
+	} break;
+
+	case TOK_TRUE:
+		lexer_next(ctx->lex);
+		return (Val){.kind = VAL_BOOL, .as.vbool = true};
+
+	case TOK_FALSE:
+		lexer_next(ctx->lex);
+		return (Val){.kind = VAL_BOOL, .as.vbool = false};
+
+	case TOK_INT: {
+		char *end;
+		return (Val){
+			.kind = VAL_INT,
+			.as.vint = strtoll(lexer_next(ctx->lex).data, &end, 0),
+		};
+	}
+
+	case TOK_FLOAT:
+		return (Val){
+			.kind = VAL_FLOAT,
+			.as.vfloat = atof(lexer_next(ctx->lex).data),
+		};
+
+	case TOK_OBRA: {
+		lexer_next(ctx->lex);
+		Val obj = eval_new_heap_val(ctx->ectx, VAL_DICT);
+		while (lexer_peek(ctx->lex).kind != TOK_CBRA) {
+			Val key = parse_json(ctx);
+			if (ctx->got_err) return key;
+
+			if (key.kind != VAL_STR)
+				return json_err(ctx, "string literal expected");
+
+			if (lexer_peek(ctx->lex).kind != TOK_COL)
+				return json_err(ctx, "column expected");
+			lexer_next(ctx->lex);
+
+			Val val = parse_json(ctx);
+			if (ctx->got_err) return val;
+
+			ValDict_add(VDICT(obj), key, val);
+
+			if (lexer_peek(ctx->lex).kind == TOK_COM) {
+				if (lexer_peek2(ctx->lex).kind == TOK_CBRA)
+					return json_err(ctx, "deprived comma");
+				lexer_next(ctx->lex);
+			} else if (lexer_peek(ctx->lex).kind != TOK_CBRA)
+				return json_err(ctx, "comma expected");
+		}
+		lexer_next(ctx->lex);
+		return obj;
+	}
+
+	case TOK_OSQBRA: {
+		lexer_next(ctx->lex);
+		Val list = eval_new_heap_val(ctx->ectx, VAL_LIST);
+		while (lexer_peek(ctx->lex).kind != TOK_CSQBRA) {
+			Val val = parse_json(ctx);
+			if (ctx->got_err) return val;
+
+			da_append(VLIST(list), val);
+
+			if (lexer_peek(ctx->lex).kind == TOK_COM) {
+				if (lexer_peek2(ctx->lex).kind == TOK_CSQBRA)
+					return json_err(ctx, "deprived comma");
+				lexer_next(ctx->lex);
+			} else if (lexer_peek(ctx->lex).kind != TOK_CSQBRA)
+				return json_err(ctx, "comma expected");
+		}
+		lexer_next(ctx->lex);
+		return list;
+	}
+
+	case TOK_STRING:
+		Val str = eval_new_heap_val(ctx->ectx, VAL_STR);
+		sb_appendf(VSTR(str), "%s", lexer_next(ctx->lex).data);
+		return str;
+	}
+
+	return json_err(ctx, "unexpected token");
+}
+
+Val JsonSerialize(EvalCtx *ctx, Location cloc, Vals args) {
+	if (args.count != 1)
+		err(ctx, cloc, "accepts only 1 argument");
+
+	if (args.items[0].kind != VAL_STR)
+		err(ctx, cloc, "expected string as first argument");
+
+	Val str = args.items[0];
+	Lexer lexer = lexer_from_str("json", VSTR(str)->items);
+
+	JsonParserCtx jctx = {
+		.ectx = ctx,
+		.lex = &lexer,
+		.got_err = false,
+	};
+
+	Val res = parse_json(&jctx);
+	if (jctx.got_err) return res;
+	return json_obj(&jctx, res);
+}
+
+Val BitXor(EvalCtx *ctx, Location cloc, Vals args) {
 	if (args.count != 2)
-		err(ctx, call_loc, "bitxor() accepts only 2 argument");
+		err(ctx, cloc, "accepts only 2 argument");
 
 	if (args.items[0].kind != VAL_INT || args.items[1].kind != VAL_INT)
-		err(ctx, call_loc, "bitxor() accepts only integers");
+		err(ctx, cloc, "accepts only integers");
 
 	return (Val){
 		.kind = VAL_INT,
@@ -95,12 +314,12 @@ Val BitXor(EvalCtx *ctx, Location call_loc, Vals args) {
 	};
 }
 
-Val Split(EvalCtx *ctx, Location call_loc, Vals args) {
+Val Split(EvalCtx *ctx, Location cloc, Vals args) {
 	if (args.count != 2)
-		err(ctx, call_loc, "split() accepts only 2 argument");
+		err(ctx, cloc, "accepts only 2 arguments");
 
 	if (args.items[0].kind != VAL_STR || args.items[1].kind != VAL_STR)
-		err(ctx, call_loc, "split() accepts only strings");
+		err(ctx, cloc, "accepts only strings");
 
 	Val str = args.items[0];
 	Val del = args.items[1];
@@ -121,9 +340,9 @@ Val Split(EvalCtx *ctx, Location call_loc, Vals args) {
 	return arr;
 }
 
-Val Int(EvalCtx *ctx, Location call_loc, Vals args) {
+Val Int(EvalCtx *ctx, Location cloc, Vals args) {
 	if (args.count != 1)
-		err(ctx, call_loc, "int() accepts only 1 argument");
+		err(ctx, cloc, "accepts only 1 argument");
 
 	Val arg = args.items[0];
 	switch (arg.kind) {
@@ -150,13 +369,13 @@ Val Int(EvalCtx *ctx, Location call_loc, Vals args) {
 			};
 		}
 
-		default: err(ctx, call_loc, "cannot convert to int");
+		default: err(ctx, cloc, "cannot convert to int");
 	}
 }
 
-Val Float(EvalCtx *ctx, Location call_loc, Vals args) {
+Val Float(EvalCtx *ctx, Location cloc, Vals args) {
 	if (args.count != 1) {
-		err(ctx, call_loc, "float() accepts only 1 argument");
+		err(ctx, cloc, "accepts only 1 argument");
 	}
 
 	Val arg = args.items[0];
@@ -167,7 +386,7 @@ Val Float(EvalCtx *ctx, Location call_loc, Vals args) {
 	case VAL_FLOAT: res = arg.as.vfloat;                  break;
 	case VAL_INT:   res = arg.as.vint;                    break;
 	case VAL_STR:   res = strtod(VSTR(arg)->items, &end); break;
-	default:        err(ctx, call_loc, "cannot convert to float");
+	default:        err(ctx, cloc, "cannot convert to float");
 	}
 
 	return (Val){
@@ -176,9 +395,9 @@ Val Float(EvalCtx *ctx, Location call_loc, Vals args) {
 	};
 }
 
-Val Rune(EvalCtx *ctx, Location call_loc, Vals args) {
+Val Rune(EvalCtx *ctx, Location cloc, Vals args) {
 	if (args.count != 1) {
-		err(ctx, call_loc, "rune() accepts only 1 argument");
+		err(ctx, cloc, "accepts only 1 argument");
 	}
 
 	Val arg = args.items[0];
@@ -187,7 +406,7 @@ Val Rune(EvalCtx *ctx, Location call_loc, Vals args) {
 	switch (arg.kind) {
 	case VAL_RUNE: res = arg.as.vrune; break;
 	case VAL_INT:  res = arg.as.vint;  break;
-	default:       err(ctx, call_loc, "cannot convert to rune");
+	default:       err(ctx, cloc, "cannot convert to rune");
 	}
 
 	return (Val){
@@ -196,9 +415,9 @@ Val Rune(EvalCtx *ctx, Location call_loc, Vals args) {
 	};
 }
 
-Val Len(EvalCtx *ctx, Location call_loc, Vals args) {
+Val Len(EvalCtx *ctx, Location cloc, Vals args) {
 	if (args.count != 1)
-		err(ctx, call_loc, "len() accepts only 1 argument");
+		err(ctx, cloc, "accepts only 1 argument");
 
 	Val arg = args.items[0];
 	long long len = 0;
@@ -207,7 +426,7 @@ Val Len(EvalCtx *ctx, Location call_loc, Vals args) {
 	case VAL_LIST: len = VLIST(arg)->count;          break;
 	case VAL_DICT: len = VDICT(arg)->count;          break;
 	case VAL_STR:  len = utf8_len(VSTR(arg)->items); break;
-	default: err(ctx, call_loc, "len() accepts only lists, strings and dictionaries");
+	default: err(ctx, cloc, "accepts only lists, strings and dictionaries");
 	}
 
 	return (Val){
@@ -216,63 +435,64 @@ Val Len(EvalCtx *ctx, Location call_loc, Vals args) {
 	};
 }
 
-Val Str(EvalCtx *ctx, Location call_loc, Vals args) {
+Val Str(EvalCtx *ctx, Location cloc, Vals args) {
 	Val str = eval_new_heap_val(ctx, VAL_STR);
-	char buf[1 << 16];
 
 	da_foreach (Val, v, &args) {
-		val_sprint(*v, buf);
-		sb_appendf(VSTR(str), "%s", buf);
+		val_sprint(*v, VSTR(str), 0);
 	}
 
 	return str;
 }
 
-Val Error(EvalCtx *ctx, Location call_loc, Vals args) {
+Val Error(EvalCtx *ctx, Location cloc, Vals args) {
 	if (args.count != 1)
-		err(ctx, call_loc, "error() accepts only 1 argument");
+		err(ctx, cloc, "accepts only 1 argument");
 	if (args.items[0].kind != VAL_STR)
-		err(ctx, call_loc, "error() accepts only string");
+		err(ctx, cloc, "accepts only string");
 
-	err(ctx, call_loc, VSTR(args.items[0])->items);
+	err(ctx, cloc, VSTR(args.items[0])->items);
 	return VNONE;
 }
 
-Val Print(EvalCtx *ctx, Location call_loc, Vals args) {
-	char buf[1 << 16];
+Val Print(EvalCtx *ctx, Location cloc, Vals args) {
+	StringBuilder sb = {0};
+
 	da_foreach (Val, it, &args) {
-		val_sprint(*it, buf);
-		printf("%s", buf);
+		val_sprint(*it, &sb, 0);
 	}
 
+	printf("%s", sb.items);
+	da_free(&sb);
+
 	return VNONE;
 }
 
-Val Println(EvalCtx *ctx, Location call_loc, Vals args) {
-	Print(ctx, call_loc, args);
+Val Println(EvalCtx *ctx, Location cloc, Vals args) {
+	Print(ctx, cloc, args);
 	printf("\n");
 	return VNONE;
 }
 
-Val Input(EvalCtx *ctx, Location call_loc, Vals args) {
+Val Input(EvalCtx *ctx, Location cloc, Vals args) {
 	if (args.items[0].kind != VAL_STR)
-		err(ctx, call_loc, "input() accepts only string");
+		err(ctx, cloc, "accepts only string");
 
 	char res[1 << 12];
 	Val str = eval_new_heap_val(ctx, VAL_STR);
 
 	printf("%s", VSTR(args.items[0])->items);
 	if (fgets(res, sizeof res, stdin) == NULL)
-		err(ctx, call_loc, "no input");
+		err(ctx, cloc, "no input");
 
 	res[strlen(res) - 1] = '\0';
 	sb_appendf(VSTR(str), "%s", res);
 	return str;
 }
 
-Val Append(EvalCtx *ctx, Location call_loc, Vals args) {
+Val Append(EvalCtx *ctx, Location cloc, Vals args) {
 	if (args.count < 2)
-		err(ctx, call_loc, "append() accepts more than 1 arguments");
+		err(ctx, cloc, "accepts more than 1 arguments");
 
 	if (args.items[0].kind == VAL_LIST) {
 		Vals *list = VLIST(args.items[0]);
@@ -281,16 +501,17 @@ Val Append(EvalCtx *ctx, Location call_loc, Vals args) {
 	} else if (args.items[0].kind == VAL_STR) {
 		StringBuilder *str = VSTR(args.items[0]);
 		for (size_t i = 1; i < args.count; i++) {
-			char buf[4096];
-			val_sprint_f(args.items[i], buf, 0);
-			sb_appendf(str, "%s", buf);
+			StringBuilder sb = {0};
+			val_sprint(args.items[i], &sb, 0);
+			sb_appendf(str, "%s", sb.items);
+			sb_free(&sb);
 		}
-	} else err(ctx, call_loc, "append() accepts only list or string");
+	} else err(ctx, cloc, "accepts only list or string");
 
 	return VNONE;
 }
 
-Val Remove(EvalCtx *ctx, Location call_loc, Vals args) {
+Val Remove(EvalCtx *ctx, Location cloc, Vals args) {
 	if (args.count != 2)
 		goto error;
 
@@ -306,23 +527,23 @@ Val Remove(EvalCtx *ctx, Location call_loc, Vals args) {
 	return VNONE;
 
 error:
-	err(ctx, call_loc, "remove() accepts list and index");
+	err(ctx, cloc, "accepts list and index");
 	return VNONE;
 }
 
-Val Range(EvalCtx *ctx, Location call_loc, Vals args) {
+Val Range(EvalCtx *ctx, Location cloc, Vals args) {
 	long long from = 0;
 	long long to   = 0;
 	long long step = 1;
 
 	if (args.count < 1 || args.count > 3) {
-		err(ctx, call_loc, "range() accepts 1, 2 and 3 arguments");
+		err(ctx, cloc, "accepts 1, 2 and 3 arguments");
 		return VNONE;
 	}
 
 	da_foreach (Val, val, &args) {
 		if (val->kind != VAL_INT) {
-			err(ctx, call_loc, "range() accepts only integers");
+			err(ctx, cloc, "accepts only integers");
 			return VNONE;
 		}
 	}
@@ -350,7 +571,7 @@ Val Range(EvalCtx *ctx, Location call_loc, Vals args) {
 	return list;
 }
 
-Val Insert(EvalCtx *ctx, Location call_loc, Vals args) {
+Val Insert(EvalCtx *ctx, Location cloc, Vals args) {
 	if (args.count != 3)
 		goto error;
 
@@ -369,12 +590,12 @@ Val Insert(EvalCtx *ctx, Location call_loc, Vals args) {
 	return VNONE;
 
 error:
-	err(ctx, call_loc, "insert() accepts: list, index and value");
+	err(ctx, cloc, "accepts: list, index and value");
 }
 
-Val Typeof(EvalCtx *ctx, Location call_loc, Vals args) {
+Val Typeof(EvalCtx *ctx, Location cloc, Vals args) {
 	if (args.count != 1)
-		err(ctx, call_loc, "typeof() accepts only 1 argument");
+		err(ctx, cloc, "accepts only 1 argument");
 
 	return (Val){
 		.kind = VAL_INT,
@@ -382,7 +603,7 @@ Val Typeof(EvalCtx *ctx, Location call_loc, Vals args) {
 	};
 }
 
-Val Has(EvalCtx *ctx, Location call_loc, Vals args) {
+Val Has(EvalCtx *ctx, Location cloc, Vals args) {
 	if (args.count != 2)
 		goto error;
 
@@ -411,24 +632,28 @@ Val Has(EvalCtx *ctx, Location call_loc, Vals args) {
 	};
 
 error:
-	err(ctx, call_loc, "has() accepts: dictionary or list, item");
+	err(ctx, cloc, "accepts: dictionary or list then item");
 	return VNONE;
 }
 
 void reg_types(EvalCtx *ctx) {
-	eval_reg_var(ctx, "_TYPE_NONE_",  (Val){.kind = VAL_INT, .as.vint = VAL_NONE});
-	eval_reg_var(ctx, "_TYPE_INT_",   (Val){.kind = VAL_INT, .as.vint = VAL_INT});
-	eval_reg_var(ctx, "_TYPE_BOOL_",  (Val){.kind = VAL_INT, .as.vint = VAL_BOOL});
+	eval_reg_var(ctx, "_TYPE_NONE_",  (Val){.kind = VAL_INT, .as.vint = VAL_NONE });
+	eval_reg_var(ctx, "_TYPE_INT_",   (Val){.kind = VAL_INT, .as.vint = VAL_INT  });
+	eval_reg_var(ctx, "_TYPE_BOOL_",  (Val){.kind = VAL_INT, .as.vint = VAL_BOOL });
 	eval_reg_var(ctx, "_TYPE_FIELD_", (Val){.kind = VAL_INT, .as.vint = VAL_FIELD});
 	eval_reg_var(ctx, "_TYPE_FLOAT_", (Val){.kind = VAL_INT, .as.vint = VAL_FLOAT});
-	eval_reg_var(ctx, "_TYPE_LIST_",  (Val){.kind = VAL_INT, .as.vint = VAL_LIST});
-	eval_reg_var(ctx, "_TYPE_DICT_",  (Val){.kind = VAL_INT, .as.vint = VAL_DICT});
-	eval_reg_var(ctx, "_TYPE_STR_",   (Val){.kind = VAL_INT, .as.vint = VAL_STR});
-	eval_reg_var(ctx, "_TYPE_RUNE_",  (Val){.kind = VAL_INT, .as.vint = VAL_RUNE});
+	eval_reg_var(ctx, "_TYPE_LIST_",  (Val){.kind = VAL_INT, .as.vint = VAL_LIST });
+	eval_reg_var(ctx, "_TYPE_DICT_",  (Val){.kind = VAL_INT, .as.vint = VAL_DICT });
+	eval_reg_var(ctx, "_TYPE_STR_",   (Val){.kind = VAL_INT, .as.vint = VAL_STR  });
+	eval_reg_var(ctx, "_TYPE_RUNE_",  (Val){.kind = VAL_INT, .as.vint = VAL_RUNE });
 }
 
 void reg_stdlib(EvalCtx *ctx) {
 	reg_types(ctx);
+
+	eval_reg_func(ctx, "json_serialize",   JsonSerialize);
+	eval_reg_func(ctx, "json_deserialize", JsonDeserialize);
+
 	eval_reg_func(ctx, "len",     Len);
 	eval_reg_func(ctx, "int",     Int);
 	eval_reg_func(ctx, "split",   Split);
